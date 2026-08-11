3fede2278cd70c3add4fb5ae631a5d11d5773aea
Nithin Bollineni <nithinbollineni04@gmail.com>
2026-08-11 11:51:28 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://library-management-system-37uz.onrender.com",
 });
 
 export default api;
